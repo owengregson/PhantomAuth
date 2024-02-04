9342116ce66afbf1e06869ca5add27708234f180
Owen Gregson <63135035+owengregson@users.noreply.github.com>
2024-02-04 13:30:59 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,116 @@
 # PhantomAuth
 A PHP Authentication System with modular resource storage, rate-limiting, ip-limiting, keys, and configuration.
+
+I've been using this authentication system for a long time in my projects but it had some major flaws and security issues that I needed to fix. After fixing everything, I've decided to release the auth here for others to use.
+PhantomAuth integrates with my other project, CMAnalytics, for powerful discord logging (if you enable that feature.)
+
+⚠️ **I am not liable for any misuse of this authentication system such as using it to 'grab IPs' or collect other user data for malicious intent. My software is provided for educational purposes only.**
+
+The Authentication System can easily manage multiple products, tiers, and users completely server-side. In addition, it includes the following features:
+* Rate-limit requests of users
+* IP-limit each key so that only one (or more) users can access it
+* Log requests to a Discord Webhook using CMAnalytics
+* Lock content behind keys so that they can only be accessed by authenticated users
+
+**All of these features are configurable**, so you can enable, disable, and change them to your liking.
+
+# Setup Guide
+
+## 1. Download the Authentication System
+You can download from the "releases" section of this repository.
+
+## 2. Import the Authentication System to your hosting provider
+If you have RDP or file-access to your website, you can simply upload the file to some directory in your website's files, e.g. `yoursite.com/auth/` (Optionally, you can rename the file to "index.php" so that it is directly accessible from the directory.)
+
+## 3. Configure the settings in the file
+Open up the `config.json` file and configure the settings to your liking. Here is an example configuration. Each setting is pretty self-explanatory.
+
+```json
+{
+    'productName': "example",
+    'iplimit': {
+        'enabled': true,
+        'ipAddressesPerKey': 2
+    },
+    'ratelimit': {
+        'enabled': true,
+        'maxRequestsPerPeriod': 5,
+        'timePeriodSeconds': 15
+    },
+    'logging': {
+        'enabled': true,
+        'webhook_url': "https://discord.com/api/webhooks/...",
+        'embed_username': "PhantomAuth",
+        'embed_color_hex': "#f44444",
+        'embed_avatar_url': "https://raw.githubusercontent.com/owengregson/PhantomAuth/main/resources/PhantomAuth.png"
+    }
+}
+```
+
+## 4. Execute the script by accessing the page
+Visit the website with no parameters in your request, e.g.
+`https://yoursite.com/auth/`
+
+Upon your first execution of the script, it will automatically propagate the necessary files and directories for your configured product name.
+
+The automatically generated file structure will look something like the following:
+
+```
+├── (productName)
+│   ├── data
+│   │   └── example-key.ip
+│   ├── keys
+│   │   └── example-keys.txt
+│   └── resources
+│       └── example-resource.txt
+└── index.php
+```
+## 5. Put in your license keys and make new tiers (if you want.)
+Access the `./keys` directory and rename the file to whatever you want your access tier to be, e.g. "premium-keys.txt" instead of "example-keys.txt"
+Then, simply add the license keys you want into that file, and they will be automatically recognized by the program. You can create new files in this format to add more tiers to your product.
+
+## 6. All finished!
+You can easily change the "productName" in the config.json to make new products and follow the setup guide inside each one once again. Any products you created previously will still authenticate just like before.
+Now that you're finished with setup, you can move on to usage of the authentication system.
+
+# Usage Guide
+
+## 1. Sending Requests
+Each request should be a GET request over HTTP or HTTPS. The URL parameters are formatted as the following:
+
+* key: The key to attempt authentication with.
+* type: The product 'tier' or type to check the key in. Must have a matching `(type)-keys.txt` file in the `./keys` directory.
+* product: The product to authenticate in.
+* request: (optional) The locked resource to access.
+
+An example authentication request using these parameters would be:
+`https://yoursite.com/auth/?key=example-key&type=example&product=example&request=example-resource`
+
+## 2. Receiving Responses
+Reponses are in the format of a JSON encoded object. The JSON object contains the following properties:
+
+* product: The product that was authenticated.
+* type: The product 'tier' or type that was checked.
+* status: Either "valid" or "invalid" indicating the result of the authentication request.
+* reason: The reason why the authentication failed (or "authorized" if it was successful.)
+* response: The requested locked data from the request property or "success" if none was provided (and authentication was successful.) Can also be "The request type did not match any resource." if the script cannot find the requested resource.
+
+An example **(successful)** authentication response would be:
+```json
+{
+    "product": "example",
+    "type": "example",
+    "status": "valid",
+    "reason": "authorized",
+    "response": "example-resource"
+}
+```
+And an example **(unsuccessful)** authentication response is:
+```json
+{
+    "product": "example",
+    "type": "example",
+    "status": "invalid",
+    "reason": "bad-request"
+}
+```
---
+++ source/PhantomAuth.php
@@ -0,0 +1,338 @@
+<!--
+==============================================================
+
+██████╗ ██╗  ██╗ █████╗ ███╗  ██╗████████╗ █████╗ ███╗   ███╗
+██╔══██╗██║  ██║██╔══██╗████╗ ██║╚══██╔══╝██╔══██╗████╗ ████║
+██████╔╝███████║███████║██╔██╗██║   ██║   ██║  ██║██╔████╔██║
+██╔═══╝ ██╔══██║██╔══██║██║╚████║   ██║   ██║  ██║██║╚██╔╝██║
+██║     ██║  ██║██║  ██║██║ ╚███║   ██║   ╚█████╔╝██║ ╚═╝ ██║
+ ═╝     ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚══╝   ╚═╝    ╚════╝ ╚═╝     ╚═╝
+
+                   Made with <3 by l3m0n
+
+=============================================================
+-->
+<font color="black" face="Verdana, Geneva, sans-serif" size="2"><?php
+
+/**
+ * PhantomAuth
+ * This software is licensed under the GNU General Public License (GPL) v3.0 or later.
+ *
+ * The GNU General Public License is a free, copyleft license published by the Free Software Foundation.
+ * It allows you to use, modify, and redistribute this software. If you redistribute the software
+ * or any modifications of it, you must also distribute the source code under the same GPL license.
+ * This ensures that the software and any improvements made to it remain free and accessible to the public.
+ * The GPL is designed to guarantee your freedom to share and change all versions of a program.
+ *
+ * For more details on the license, please visit https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+// Load and parse the configuration from config.json
+$config = json_decode((file_get_contents(dirname(__FILE__) . '/config.json')), true);
+
+// Enable CORS for cross-origin requests
+header("Access-Control-Allow-Origin: *");
+
+// Get CMAnalytics Library (if logging is enabled.)
+if($config['logging']['enabled']) {
+    $CMAnalytics = str_replace("?>", "", str_replace("<?php", "", file_get_contents("https://raw.githubusercontent.com/owengregson/CMAnalytics/main/minified/CMAnalytics.php")));
+    eval($CMAnalytics);
+}
+
+/**
+ * Writes data to a file, creating the file if it does not exist.
+ * 
+ * @param string $filePath The path to the file to be written.
+ * @param string $content The content to be written to the file.
+ * @param boolean $append Whether to append content or overwrite the file.
+ * @param boolean $private Whether or not the file is only accessible by the script
+ */
+function writeFile($filePath, $content, $append = false, $private = true) {
+    $flags = $append ? FILE_APPEND : 0;
+    file_put_contents($filePath, $content, $flags);
+    
+    // Make the file private if necessary
+    if ($private) {
+        chmod($filePath, 0700);
+    }
+}
+
+/**
+ * Reads file data into an array. Each line of the file becomes an element in the array.
+ * 
+ * @param string $filePath The path to the file to be read.
+ * @return array|false The lines of the file in an array, or false if the file cannot be read.
+ */
+function readFileIntoArray($filePath) {
+    if (!file_exists($filePath)) {
+        return false;
+    }
+
+    // Split the decrypted content into lines
+    $lines = explode(PHP_EOL, trim(file_get_contents($filePath)));
+    return $lines;
+}
+
+/**
+ * Checks and returns the content of a resource file if it matches the request type.
+ * 
+ * @param string $requestType The request type to match against filenames in the resources directory.
+ * @return string|false The content of the matched file or false if no match is found.
+ */
+function getResourceContent($requestType, $product) {
+    $productPath = dirname(__FILE__) . '/' . $product;
+    $resourcesPath = $productPath . '/resources';
+    $files = scandir($resourcesPath);
+    if($requestType =="validate" || $requestType == "" || is_null($requestType)) return "success";
+    foreach ($files as $file) {
+        // Remove file extension
+        $filenameWithoutExtension = pathinfo($file, PATHINFO_FILENAME);
+
+        // Compare request type with filename
+        if (strtolower($requestType) === strtolower($filenameWithoutExtension)) {
+            $filePath = $resourcesPath . '/' . $file;
+            return file_get_contents($filePath);
+        }
+    }
+
+    return false;
+}
+
+/**
+ * Initializes directories and files required for the application.
+ * Creates 'data' and 'keys' directories in the specified product directory.
+ * Also creates 'example-keys.txt' in the 'keys' directory.
+ * 
+ * @param string $product The name of the product directory.
+ */
+function initializeDirectories($product) {
+    $productPath = dirname(__FILE__) . '/' . $product;
+    $dataPath = $productPath . '/data';
+    $resourcesPath = $productPath . '/resources';
+    $keysPath = $productPath . '/keys';
+    $exampleResourceFile = $resourcesPath . "/example-resource.txt";
+    $exampleKeysFile = $keysPath . "/example-keys.txt";
+
+    if (!is_dir($dataPath)) {
+        mkdir($dataPath, 0700, true);
+    }
+    
+    if (!is_dir($resourcesPath)) {
+        mkdir($resourcesPath, 0700, true);
+        if(!file_exists($exampleResourceFile)) {
+            //Initialize the file with default content using writeFile
+            writeFile($exampleResourceFile, "example-resource", false);
+        }
+    }
+
+    if (!is_dir($keysPath)) {
+        mkdir($keysPath, 0700, true);
+        if (!file_exists($exampleKeysFile)) {
+            // Initialize the file with default content using writeFile
+            writeFile($exampleKeysFile, "example-key\n", false);
+        }
+    }
+}
+
+/**
+ * Enforces a rate limit for requests per IP address, extending the rate limit if requests continue.
+ * 
+ * @param int $maxRequests Maximum number of requests allowed within the time interval.
+ * @param int $rateLimitInterval Time interval in seconds.
+ * @return boolean Whether or not the request has been rate-limited.
+ */
+function checkRateLimit($maxRequests, $rateLimitInterval) {
+    global $config; // Ensure access to the global configuration
+    
+    // If rate-limiting is disabled immediately exit
+    if(!$config['ratelimit']['enabled']) return false;
+    
+    session_start();
+
+    // Identify client IP
+    $clientIp = $_SERVER['REMOTE_ADDR'];
+
+    // Session keys for IP-based tracking
+    $sessionRequestKey = $clientIp . '_last_request';
+    $sessionCountKey = $clientIp . '_request_count';
+    $sessionRateLimitExtendedKey = $clientIp . '_rate_limit_extended';
+
+    // Initialize or reset rate limit extension flag
+    if (!isset($_SESSION[$sessionRateLimitExtendedKey])) {
+        $_SESSION[$sessionRateLimitExtendedKey] = false;
+    }
+
+    // Initialize session for new IP
+    if (!isset($_SESSION[$sessionRequestKey])) {
+        $_SESSION[$sessionRequestKey] = time();
+        $_SESSION[$sessionCountKey] = 0;
+    }
+
+    // Check rate limit status
+    if (time() - $_SESSION[$sessionRequestKey] < $rateLimitInterval) {
+        if ($_SESSION[$sessionCountKey] >= $maxRequests) {
+            // If rate-limited, extend the penalty period for each request
+            $_SESSION[$sessionRequestKey] = time(); // Reset the last request time to now
+            $_SESSION[$sessionRateLimitExtendedKey] = true; // Flag to indicate rate limit was extended
+            return true; // Rate-limited
+        } else {
+            $_SESSION[$sessionCountKey]++;
+        }
+    } else {
+        // If the current time is outside the rate limit interval
+        if ($_SESSION[$sessionRateLimitExtendedKey]) {
+            // If rate limit was previously extended, check if we should still block
+            if (time() - $_SESSION[$sessionRequestKey] < $rateLimitInterval) {
+                return true; // Still rate-limited due to extension
+            }
+            // Reset after the extended period has elapsed without new requests
+            $_SESSION[$sessionRateLimitExtendedKey] = false;
+        }
+        // Reset for a new interval
+        $_SESSION[$sessionCountKey] = 1;
+        $_SESSION[$sessionRequestKey] = time();
+    }
+    return false; // Not rate-limited
+}
+
+/**
+ * Checks and manages IP address limitations for a given key and product.
+ * 
+ * @param string $key The key to check.
+ * @param string $prod The product associated with the key.
+ * @param integer $ipAPK The amount of unique ip addresses that can access that key.
+ * @return array An array containing a boolean status and a reason string.
+ */
+function checkIPLimit($key, $prod, $ipAPK) {
+    global $config; // Ensure access to the global configuration
+    
+    // If IP-Limiting is not enabled, immediately exit
+    if(!$config['iplimit']['enabled']) return [true, "authorized"];
+    
+    $filePath = sprintf("./%s/data/%s.ip", $prod, $key);
+    $userip = $_SERVER['HTTP_CLIENT_IP'] ?? $_SERVER['HTTP_X_FORWARDED_FOR'] ?? $_SERVER['REMOTE_ADDR'];
+
+    // Attempt to read the existing IPs from the file
+    $ips = readFileIntoArray($filePath);
+
+    // If the file does not exist or cannot be read, start with an empty array
+    if ($ips === false) {
+        $ips = [];
+    }
+
+    // Check if the user's IP is already in the list or if the IP limit has not been reached
+    if (!in_array($userip, $ips) && count($ips) < $ipAPK) {
+        // Append the new IP address since it's not in the list and the limit hasn't been reached
+        $ips[] = $userip;
+        // Write the updated list of IPs back to the file
+        writeFile($filePath, $userip . PHP_EOL, true); // Appending the IP
+        return [true, "authorized"];
+    } elseif (in_array($userip, $ips)) {
+        // If the IP is already in the list, consider it authorized without modifying the file
+        return [true, "authorized"];
+    }
+
+    // If the function hasn't returned by now, it means the IP limit has been reached
+    return [false, "ip-limit"];
+}
+
+/**
+ * Authenticates a key for a given product and type.
+ * 
+ * @param string $product The product to authenticate against.
+ * @param string $key The key to authenticate.
+ * @param string $type The type of key.
+ * @return object An object containing product, type, status, and reason properties.
+ */
+function checkKeyAuth($product, $key, $type) {
+    global $config; // Ensure access to the global configuration
+
+    if (!checkRateLimit($config['ratelimit']['maxRequestsPerPeriod'], $config['ratelimit']['timePeriodSeconds'])) {
+        $keysExisting = readFileIntoArray("./$product/keys/$type-keys.txt");
+        if ($keysExisting !== false && in_array($key, $keysExisting)) {
+            $cIPResult = checkIPLimit($key, $product, $config['iplimit']['ipAddressesPerKey']);
+            $status = $cIPResult[0] ? "valid" : "invalid";
+            $reason = $cIPResult[1];
+        } else {
+            $reason = "bad-request";
+        }
+    } else {
+        $reason = "ratelimit";
+    }
+    return (object) ['product' => $product, 'type' => $type, 'status' => $status ?? "invalid", 'reason' => $reason];
+}
+
+/**
+ * Use CMAnalytics to log request to a discord webhook.
+ * 
+ * @param array $request The parameters sent in the request.
+ */
+function logRequest($request, $authResult) {
+    global $config; // Ensure access to the global configuration.
+    global $CMAnalytics; // Ensure access to the CMAnalytics library.
+
+    if($config['logging']['enabled'] && !is_null($CMAnalytics) && !is_null($request) && !is_null($authResult)) {
+        try {
+            CMAnalyticsV2($config['logging']['webhook_url'], $config['logging']['embed_username'], $config['logging']['embed_avatar_url'], "PhantomAuth Request", "", $config['logging']['embed_color_hex'], [["name" => "Request Parameters", "value" => strval(json_encode($request, JSON_PRETTY_PRINT)), "inline" => false], ["name" => "Authentication Result", "value" => strval(json_encode($authResult, JSON_PRETTY_PRINT)), "inline" => false]]);
+        } catch(Exception $e) {
+            // Failed to log to Discord Webhook.
+        }
+    }
+}
+
+/**
+ * Outputs a JSON response based on the provided object.
+ * 
+ * @param object $responseObj An object containing the response properties.
+ */
+function outputJsonResponse($responseObj) {
+    echo json_encode($responseObj, JSON_PRETTY_PRINT);
+}
+
+/**
+ * Sanitize URL parameters to prevent security vulnerabilities.
+ * 
+ * @param array $params The parameters to sanitize.
+ * @return array The sanitized parameters.
+ */
+function sanitizeUrlParams($params) {
+    $cleanParams = [];
+    foreach ($params as $key => $value) {
+        // Using filter_var with FILTER_SANITIZE_STRING to remove tags and encode special characters
+        $cleanParams[$key] = filter_var($value, FILTER_SANITIZE_STRING);
+    }
+    return $cleanParams;
+}
+
+// Initialize necessary directories for the product
+initializeDirectories($config['productName']);
+
+// Parse URL to get user parameters
+$url = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+$url_components = parse_url($url);
+parse_str($url_components['query'], $params);
+
+// Sanitize URL parameters
+$sanitizedParams = sanitizeUrlParams($params);
+
+// Check Authentication for this request
+$authResult = checkKeyAuth($sanitizedParams['product'], $sanitizedParams['key'], $sanitizedParams['type']);
+
+// Fetch the requested resource (if the authentication was successful)
+if ($authResult->{'status'} == "valid") {
+    // Attempt to get content from a resource file that matches the request type
+    $resourceContent = getResourceContent($sanitizedParams['request'], $sanitizedParams['product']);
+    if ($resourceContent !== false) {
+        $authResult->{'response'} = $resourceContent;
+    } else {
+        $authResult->{'response'} = "Request type does not match any resource.";
+    }
+}
+
+// Log to Discord Webhook
+logRequest($sanitizedParams, $authResult);
+
+// Output the response to the user
+outputJsonResponse($authResult);
+
+?></font>
